6bbc3d772a2866e41567661fdbb6794dd5d7bc9e
wentao0420 <103408489+wentao0420@users.noreply.github.com>
2022-11-02 17:10:04 +0800

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,13 +1,13 @@
-class cat:
-    def __init__(self,name):
+class Cat(object):
+    '''A Cat with name, and greets each other.'''
+
+    def __init__(self, name):
+        '''A new cat with name value name.'''
         self.name = name
-    def greet(self , anothercat):
-        self.anothercat = anothercat
-        print(f"Hello I am {self.name} ! I see you are also a cool fluffy kitty {anothercat.get_name()} , let's together purr at the human, so that they shall give us food")
 
-    def get_name(self):
-        return self.name
-
-# cat1 = cat("Kittosaurus Rex")
-# cat2 = cat("Snowball IX")
-# cat1.saying_hi(cat2)
+    def greet(self, another_name):
+        '''Print the name of the cat with greeting.'''
+        print (f"Hello! I am {self.name}! Nice to meet you {another_name}! Let's together purr at the human, so that they shall give us food!")
+        
+Hercules = Cat("Hercules")
+Hercules.greet("Tim")
